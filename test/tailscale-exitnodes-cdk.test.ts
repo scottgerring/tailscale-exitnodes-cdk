9befcad4205adce41feeea587088009806eb9a18
Scott Gerring <gerrings@amazon.com>
2023-04-09 21:10:46 +0800

    Initial commit

---
+++ test/tailscale-exitnodes-cdk.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as TailscaleExitnodesCdk from '../lib/tailscale-exitnodes-cdk-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/tailscale-exitnodes-cdk-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new TailscaleExitnodesCdk.TailscaleExitnodesCdkStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
