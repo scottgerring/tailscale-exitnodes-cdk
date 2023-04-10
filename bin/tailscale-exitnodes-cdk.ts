be079e1e3d3cb714c7fff7a14dc4c3d34ab10e7a
Scott Gerring <gerrings@amazon.com>
2023-04-10 14:44:42 +0800

    Initial commit

--- bin/tailscale-exitnodes-cdk.ts
+++ bin/tailscale-exitnodes-cdk.ts
@@ -1,21 +1,23 @@
 #!/usr/bin/env node
 import 'source-map-support/register';
 import * as cdk from 'aws-cdk-lib';
-import { TailscaleExitnodesCdkStack } from '../lib/tailscale-exitnodes-cdk-stack';
+import { TailscaleExitnodesCdkStack, ExitNodeProps } from '../lib/tailscale-exitnodes-cdk-stack';
 
 const app = new cdk.App();
-new TailscaleExitnodesCdkStack(app, 'TailscaleExitnodesCdkStack', {
-  /* If you don't specify 'env', this stack will be environment-agnostic.
-   * Account/Region-dependent features and context lookups will not work,
-   * but a single synthesized template can be deployed anywhere. */
 
-  /* Uncomment the next line to specialize this stack for the AWS Account
-   * and Region that are implied by the current CLI configuration. */
-  // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
+const tailscaleAuthKey = process.env.TAILSCALE_AUTH_KEY!
 
-  /* Uncomment the next line if you know exactly what Account and Region you
-   * want to deploy the stack to. */
-  // env: { account: '123456789012', region: 'us-east-1' },
+const stackForRegion = function(id: string, region: string, exitNodeName: string) {
+  return new TailscaleExitnodesCdkStack(app, id, {
+    tailscaleAuthKey,
+    exitNodeName,
+    env: {
+      region: region
+    }
+  })
+}
 
-  /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
-});
+const stacks = [
+  stackForRegion('ExitNodesStackSydney', 'ap-southeast-2', "TSSydneyExitNode"),
+  stackForRegion('ExitNodesStackZurich', 'eu-central-2', "TSZurichExitNode")
+];
--- lib/tailscale-exitnodes-cdk-stack.ts
+++ lib/tailscale-exitnodes-cdk-stack.ts
@@ -1,16 +1,69 @@
 import * as cdk from 'aws-cdk-lib';
+import { CloudFormationInit, InitCommand, InitFile, Instance, InstanceType, IpAddresses, MachineImage, OperatingSystemType, SubnetType, UserData, Vpc } from 'aws-cdk-lib/aws-ec2';
 import { Construct } from 'constructs';
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
+export interface ExitNodeProps extends cdk.StackProps {
+  tailscaleAuthKey: string,
+  exitNodeName: string
+}
+
 export class TailscaleExitnodesCdkStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+
+  constructor(scope: Construct, id: string, props: ExitNodeProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    // Create a VPC for our node to live in
+    const vpc = new Vpc(this, 'TheVPC', {
+      ipAddresses: IpAddresses.cidr('10.0.0.0/16'),
+      natGateways: 0,
+      subnetConfiguration: [
+        {
+          cidrMask: 24,
+          name: 'tailscale',
+          subnetType: SubnetType.PUBLIC,
+        },
+      ]
+    })
+    
+    // Drop our node into the public subnet to keep things simple (and save needing NAT!)
+    const publicSubnets = vpc.selectSubnets({
+      subnetType: SubnetType.PUBLIC
+    });    
 
-    // example resource
-    // const queue = new sqs.Queue(this, 'TailscaleExitnodesCdkQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    // Get the latest ubuntu 22.04 machine image
+    const machineImage = MachineImage.fromSsmParameter(
+      '/aws/service/canonical/ubuntu/server/jammy/stable/current/amd64/hvm/ebs-gp2/ami-id',
+    );
+    
+    const userData = UserData.forLinux()
+    userData.addCommands(
+      // Enable ip forwarding 
+      "echo 'net.ipv4.ip_forward = 1' | sudo tee -a /etc/sysctl.conf",
+      "echo 'net.ipv6.conf.all.forwarding = 1' | sudo tee -a /etc/sysctl.conf",
+      "sysctl -p /etc/sysctl.conf",
+
+      // Install tailscale
+      "curl -fsSL https://tailscale.com/install.sh | sh",
+      `tailscale up --authkey ${props.tailscaleAuthKey} --advertise-exit-node --hostname=${props.exitNodeName}`
+    );
+
+    // Create our exit node
+    const instance = new Instance(this, "exitNode", {      
+      instanceType: new InstanceType("t3.micro"),
+      vpc: vpc,
+      instanceName: props.exitNodeName,
+      vpcSubnets: publicSubnets,
+      machineImage: machineImage,
+      // init: CloudFormationInit.fromElements(
+      //   InitFile.fromAsset("/tmp/install-tailscale.sh", "install-tailscale.sh", {
+      //     mode: '000755' // Make it executable
+      //   }),
+      //   //InitCommand.argvCommand(['/tmp/install-tailscale.sh', props.tailscaleAuthKey], {}),
+      // ),
+      userData: userData
+    });
   }
+
 }
+
